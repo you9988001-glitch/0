6d7d13b355984f83a22e16c5b37c1128498abf5e
you9988001-glitch <you9988001@gmail.com>
2026-02-08 04:32:27 +0900

    Refactor issue guardian to use counter variable

--- api/issue-guardian.js
+++ api/issue-guardian.js
@@ -1,18 +1,12 @@
-// Vercel Serverless Function
-let count = 0;
+let counter = 1;
 
 export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'POST only' });
-  }
+  counter += 1;
 
-  count += 1;
+  const code =
+    `CODE-ARCHE-2026-THIA-${String(counter).padStart(4,'0')}`;
 
-  // 개발자가 "서버가 알고 있는 숫자"를 확인하는 유일한 증거
-  console.log('[ISSUE-GUARDIAN] current count =', count);
+  console.log('ISSUED:', code);
 
-  res.status(200).json({
-    ok: true,
-    issuedNumber: count
-  });
+  res.status(200).json({ code });
 }
